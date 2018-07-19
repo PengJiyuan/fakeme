0da29e633738fc732bbbe9360bfb04dee0a89150
PengJiyuan <731401082@qq.com>
2018-07-19 15:00:37 +0800

    add name test

--- test/name.test.js
+++ test/name.test.js
@@ -3,11 +3,26 @@
 const fakeData = require('../lib/fakedata.cjs');
 const fd = new fakeData();
 
-test('test random name (zh)', () => {
-  const expected = [
-    expect.stringMatching(/[\\u4E00-\\u9FFF]+/g),
-  ];
-  expect(fd.name({lang: 'zh'})).toEqual(
-    expect.arrayContaining(expected),
-  );
+describe('test random name.', () => {
+  it('matches if random name is language zh-CN.', () => {
+    const expected = [
+      // match chinese output
+      expect.stringMatching(/[\u4E00-\u9FFF]+/)
+    ];
+    const randomName = fd.name({lang: 'zh', count: 10});
+    expect(randomName).toEqual(
+      expect.arrayContaining(expected)
+    );
+  });
+
+  it('matches if random name is language english.', () => {
+    const expected = [
+      // match english output
+      expect.stringMatching(/^[a-zA-Z]+/)
+    ];
+    const randomName = fd.name({lang: 'en', count: 10});
+    expect(randomName).toEqual(
+      expect.arrayContaining(expected)
+    ); 
+  });
 });
